24da0ace8b4f1199bca096a097323d767fb59f6d
Tim Dove <timdoveemail@yahoo.com>
2025-01-12 13:41:47 -0500

    Init

---
+++ src/app/error..tsx
@@ -0,0 +1,31 @@
+"use client"
+
+import React from "react"
+interface ErrorProps {
+  error: Error
+  reset: () => void
+}
+
+const GlobalError: React.FC<ErrorProps> = ({ error, reset }) => {
+  return (
+    <html>
+      <head>
+        <title>Application Error</title>
+      </head>
+      <body>
+        <div style={{ padding: "2rem", textAlign: "center" }}>
+          <h1>Something went wrong!</h1>
+          <p>{error.message}</p>
+          <button
+            onClick={reset}
+            style={{ marginTop: "1rem", padding: "0.5rem 1rem" }}
+          >
+            Try Again
+          </button>
+        </div>
+      </body>
+    </html>
+  )
+}
+
+export default GlobalError
